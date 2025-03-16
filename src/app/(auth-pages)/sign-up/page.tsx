aa7742a61b1a91b78b9edb197f4129d22e3dd483
Samuel <samueltomar@hotmail.com>
2025-03-16 12:57:19 +0100

    update : fixes

--- src/app/(auth-pages)/sign-up/page.tsx
+++ src/app/(auth-pages)/sign-up/page.tsx
@@ -1,11 +1,11 @@
-import { signUpAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
-import { SubmitButton } from "@/components/submit-button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { signUpAction } from "../../actions";
+import { FormMessage, Message } from "../../../components/form-message";
+import { SubmitButton } from "../../../components/submit-button";
+import { Input } from "../../../components/ui/input";
+import { Label } from "../../../components/ui/label";
 import Link from "next/link";
 import { SmtpMessage } from "../smtp-message";
-
+  
 export default async function Signup(props: {
   searchParams: Promise<Message>;
 }) {
